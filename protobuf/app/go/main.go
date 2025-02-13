f771e31efb11eb27d5478b21d512d56152bcdced
hamburger <1249476060@qq.com>
2025-02-13 17:37:14 +0800

    feat: specify go version as 1.22.3

---
+++ protobuf/app/go/main.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"fmt"
+	"log"
+
+	"protobuf/entity"
+
+	"google.golang.org/protobuf/proto"
+)
+
+func main() {
+	ch := &entity.Chapter{
+		ChapterId: "1",
+		Name:      "Introduction",
+		CoverUrl:  "https://example.com/cover.jpg",
+		InitDialogue: []*entity.Dialogue{
+			{Name: "Alice", Index: 1, Message: "Hello!"},
+		},
+	}
+
+	data, err := proto.Marshal(ch)
+	if err != nil {
+		log.Fatalf("Failed to marshal: %v", err)
+	}
+
+	fmt.Println("Serialized data:", string(data))
+
+	newCh := &entity.Chapter{}
+	if err := proto.Unmarshal(data, newCh); err != nil {
+		log.Fatalf("Failed to unmarshal: %v", err)
+	}
+
+	fmt.Printf("Deserialized Chapter: %+v\n", newCh)
+}
